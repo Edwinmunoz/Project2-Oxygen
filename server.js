6cbb7efe68d87850aa426a45857094753e3381df
Jennifer Funchess <jfunchess@ymail.com>
2020-12-09 17:56:42 -0600

    set up initial server.js and downloaded npm packages

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const db = require("./models");
+const app = express();
