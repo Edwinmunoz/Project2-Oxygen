cdde6dc337f901a0a16329218d574491da9b18d0
Jennifer Funchess <jfunchess@ymail.com>
2020-12-12 01:31:12 -0600

    updated server.js handlebars connection

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const {
 } = require("@handlebars/allow-prototype-access");
 const db = require("./models");
 const app = express();
+
+app.use(express.static("public"));
+
+// Configure express-handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+  })
+);
+app.set("view engine", "handlebars");
