c9af9b52fbba5c01cde5b4a2c205f569f5ec706a
Jennifer Funchess <jfunchess@ymail.com>
2020-12-16 17:26:11 -0600

    connected all links on navbar

--- Routes/api-index.js
+++ Routes/api-index.js
@@ -2,24 +2,23 @@
 var path = require("path");
 
 // Routes
-module.exports = function(app) {
-
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../layouts/home.html"));
+  app.get("/home", function (req, res) {
+    res.sendFile(path.join(__dirname, "./views/home.handlebars"));
   });
 
-  
-  app.get("/giver", function(req, res) {
-    res.sendFile(path.join(__dirname, "../layouts/giver.html"));
+  app.get("/giver", function (req, res) {
+    res.sendFile(path.join(__dirname, "./views/giver.handlebars"));
   });
 
-  
-  app.get("/receiver", function(req, res) {
-    res.sendFile(path.join(__dirname, "../layouts/receiver.html"));
+  app.get("/receiver", function (req, res) {
+    res.sendFile(path.join(__dirname, "./views/receiver.handlebars"));
   });
 
-
+  app.get("/aboutus", function (req, res) {
+    res.sendFile(path.join(__dirname, "./views/aboutus.handlebars"));
+  });
 };
--- controllers/receiverController.js
+++ controllers/receiverController.js
@@ -5,39 +5,34 @@ const db = require("../models");
 
 // on receiver button is pressed on home.handlebars then it will direct to receiver.handlerbars
 router.get("/receiver/new", (req, res) => {
-    res.render("receiver");
-   });
+  res.render("receiver");
+});
 
 // when Contact is pressed on Nav bar then it will direct to contact.handlerbars
 router.get("/contactus", (req, res) => {
-    res.render("contact");
-   });
+  res.render("contact");
+});
 
 // when About us is pressed on Nav bar then it will direct to aboutus.handlerbars
 router.get("/aboutus", (req, res) => {
-    res.render("aboutus");
-   });
-   
-   
+  res.render("aboutus");
+});
+
+router.get("/giver", (req, res) => {
+  res.render("giver");
+});
+
 // this is when new receiver is submitted then it will retreive data from the body and entered to our database
- 
-  router.post("/api/receivers", (req, res) => {
-    db.Gift.create(req.body)
-      .then((newReceiver) => {
-        console.log(newReceiver);
-        res.json(newReceiver);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
 
-  
+router.post("/api/receivers", (req, res) => {
+  db.Gift.create(req.body)
+    .then((newReceiver) => {
+      console.log(newReceiver);
+      res.json(newReceiver);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-
-
-
-
-   
-
-   module.exports = router;
+module.exports = router;
