c3420d0e85924a2464f61a4f701eaa75bd45fbf9
edwinmunoz <edwin97munoz@gmail.com>
2020-12-16 18:54:09 -0500

    delete and update methoded added

--- controllers/receiverController.js
+++ controllers/receiverController.js
@@ -20,7 +20,15 @@ router.get("/aboutus", (req, res) => {
    
    
 // this is when new receiver is submitted then it will retreive data from the body and entered to our database
- 
+  // router.get("/api/receivers", function(req, res) {
+  //   db.Gift.findAll({
+  //     include: [db.Post]
+  //   }).then(function(newReceiver) {
+  //     res.json(newReceiver);
+  //   });
+  // });
+
+
   router.post("/api/receivers", (req, res) => {
     db.Gift.create(req.body)
       .then((newReceiver) => {
@@ -31,6 +39,24 @@ router.get("/aboutus", (req, res) => {
         console.log(err);
       });
   });
+
+  router.get("/api/receivers", function(req, res) {
+  });
+
+  router.delete("api/receivers/:id", function (req, res) {
+    db.gifts_list.destroy({where: {id: req.params.id}})
+    .then(function (dbGift) {
+        res.json(dbGift);
+    });
+});
+
+router.put("api/updateReciever/:id", function (req, res) {   
+  console.log(req.body)       
+  db.giftsList.update(req.body, { where: { id: req.params.id }})
+  .then(function (updateReciever) {
+      res.json(updateReciever);
+  })
+});
 
   
 
