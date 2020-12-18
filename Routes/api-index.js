44566ce404207f9ae69f5a72c1d93f39e0fb0103
Joseph Strickland <jks2090@outlook.com>
2020-12-18 14:45:43 -0500

    heroku updated

--- Routes/api-index.js
+++ Routes/api-index.js
@@ -6,8 +6,24 @@ module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
+  // app.get("/home", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "./views/home.handlebars"));
+  // });
+
+  // app.get("/giver", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "./views/giver.handlebars"));
+  // });
+
+  // app.get("/new", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "./views/receiver.handlebars"));
+  // });
+
+  // app.get("/aboutus", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "./views/aboutus.handlebars"));
+  // });
+
   app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, "./views/home.handlebars"));
+    res.json(path.join(__dirname, "./views/home.handlebars"));
   });
 
   app.get("/giver", function (req, res) {
