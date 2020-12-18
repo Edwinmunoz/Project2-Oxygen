0061bc799cd33a844518cde437d7efbad11a926a
Jennifer Funchess <jfunchess@ymail.com>
2020-12-17 21:45:17 -0500

    fixed contact us link after merge break

--- controllers/receiverController.js
+++ controllers/receiverController.js
@@ -6,22 +6,18 @@ const db = require("../models");
 //------------------------------------------------------------------
 // list of all records that does not have Giver, this will use giver-list handlebar
 // this will handle Delect and Edit function from Giver
-router.get("/list", function(req, res) {
-
+router.get("/list", function (req, res) {
   db.Gift.findAll({
     //This will filed all data without Giver Name
     where: {
       Giver: null,
     },
-    order: [["id", "desc"]]
-
-  }).then(function(results) {
-    res.render("giver-list",{gifts: results})
+    order: [["id", "desc"]],
+  }).then(function (results) {
+    res.render("giver-list", { gifts: results });
   });
-
 });
 //--------------------------------------------------------------
-
 
 // 1) post the new receiver created and intertit to the database
 router.post("/api/receiver", (req, res) => {
@@ -29,18 +25,24 @@ router.post("/api/receiver", (req, res) => {
     .then((newReceiver) => {
       console.log(newReceiver);
       res.json(newReceiver);
-      console.log(newReceiver.id)
-      
+      console.log(newReceiver.id);
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-<<<<<<< HEAD
 // when About us is pressed on Nav bar then it will direct to aboutus.handlerbars
 router.get("/aboutus", (req, res) => {
   res.render("aboutus");
+});
+
+router.get("/contactus", (req, res) => {
+  res.render("contact");
+});
+
+router.get("/giver", (req, res) => {
+  res.render("giver");
 });
 
 // this is when new receiver is submitted then it will retreive data from the body and entered to our database
@@ -80,69 +82,6 @@ router.delete("api/receivers/:id", function (req, res) {
 //       res.json(updateReciever);
 //   })
 // });
-=======
-
-//-------------------------------------------------------------------------
-
-//this will find the record Giver want to add giver information
-//this will use id parames from updateReceiver.js submit click 
-//this will use giverUpdated handlebar
-router.get("/giver/:id", (req, res) => {
-  db.Gift.findOne({
-    where: {
-      id: req.params.id,
-    },
-  }).then((foundReceiver) => {
-    console.log(foundReceiver.email);
-    res.render("giverUpdated", {
-      Email: foundReceiver.Email,
-      Receiver: foundReceiver.Receiver,
-      Giver: foundReceiver.Giver,
-      Address: foundReceiver.Address,
-      Gift1: foundReceiver.Gift1,
-      Gift2: foundReceiver.Gift2,
-      Gift3: foundReceiver.Gift3,
-      id: foundReceiver.id,
-    });
-  });
-});
-//------------------------------------------------------------------
-
-// This will update the Giver name and Email 
-
-router.put("/api/giver/:id", (req, res) => {
-  db.Gift.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((updateGiver) => {
-      res.json(updateGiver);
-      console.log(updateGiver)
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-//-------------------------------------------------------------------
-//this will update the receiver name, address, gift1, gift2, gift3
-router.put("/api/receiver/:id", (req, res) => {
-  db.Gift.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((updateReceiver) => {
-      res.json(updateReceiver);
-      console.log(updateReceiver)
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
->>>>>>> 46945869af8acc8d28d0871b5a428f8c28563902
 
 //-------------------------------------------------------------------
 
@@ -163,7 +102,6 @@ router.delete("/api/giver/:id", (req, res) => {
     });
 });
 
-
 //-------------------------------------------------------------------
 // this will delete record from database when Delect button is clicked from /new
 router.delete("/api/receiver/:id", (req, res) => {
@@ -181,24 +119,15 @@ router.delete("/api/receiver/:id", (req, res) => {
     });
 });
 
-
-<<<<<<< HEAD
-=======
-
 //---------------------------------------------------------------------
-// 2) This will show all data and sorted by Id - lates will post to top 
+// 2) This will show all data and sorted by Id - lates will post to top
 
 router.get("/new", (req, res) => {
   db.Gift.findAll({
-    order: [["id", "desc"]]
-  })
-    .then(function(results) {
-    res.render("receiver",{gifts: results})
-  })
+    order: [["id", "desc"]],
+  }).then(function (results) {
+    res.render("receiver", { gifts: results });
+  });
 });
 
-
->>>>>>> 46945869af8acc8d28d0871b5a428f8c28563902
 module.exports = router;
-
-
